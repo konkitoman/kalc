05a3139f727b6ba820192975863700eaeb23265e
Konkitoman <konkitoman@users.noreply.github.com>
2023-06-07 15:43:00 +0300

    Now spits out the result directly

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
     let args = std::env::args().skip(1).collect::<String>();
-    println!("Res: {}", kalc::kalc_f64(&args).unwrap())
+    println!("{}", kalc::kalc_f64(&args).unwrap())
 }
