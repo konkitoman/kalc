bbc307051651da9c724661aaedd76ec591a500a1
Konkitoman <konkitoman@users.noreply.github.com>
2023-06-07 15:40:43 +0300

    Add kalc, kalc_i64, kalc_f64

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
-pub mod executor;
-pub mod lexer;
-pub mod token;
+mod executor;
+mod lexer;
+mod token;
+
+pub use executor::Executor;
+pub use lexer::Lexer;
+pub use token::Token;
+
+pub fn kalc(formula: &str) -> Result<Executor, String> {
+    let mut lexer = Lexer::default();
+    lexer.parse(formula)?;
+    let mut executor = Executor::new(lexer);
+    executor.execute();
+    Ok(executor)
+}
+
+pub fn kalc_i64(formula: &str) -> Result<i64, String> {
+    Ok(kalc(formula)?.get_i64())
+}
+
+pub fn kalc_f64(formula: &str) -> Result<f64, String> {
+    Ok(kalc(formula)?.get_f64())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,4 @@
-use kalc::{executor::Executor, lexer::Lexer};
-
 fn main() {
-    let mut lexer = Lexer::default();
     let args = std::env::args().skip(1).collect::<String>();
-    println!("Args: {args}");
-    lexer.parse(&args).unwrap();
-    println!("Lexer: {lexer:?}");
-    let mut executor = Executor::new(lexer);
-    executor.execute();
-    let resi = executor.get_i64();
-    let resf = executor.get_f64();
-    println!("Res i: {resi}");
-    println!("Res f: {resf}");
+    println!("Res: {}", kalc::kalc_f64(&args).unwrap())
 }
