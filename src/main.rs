e654e4c7a42466b2047ac9f0498f4e12405ef048
Konkitoman <konkitoman@users.noreply.github.com>
2023-07-13 16:25:58 +0300

    Fix an error because of the rename

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let args = std::env::args().skip(1).collect::<String>();
-    match kalc::kalc_f64(&args) {
+    match kalc_kman::kalc_f64(&args) {
         Ok(res) => {
             println!("{res}")
         }
